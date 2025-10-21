f3c109092b06bb62e297894d3b9fd85e3bab48b1
azey <me@azey.net>
2025-10-21 07:07:20 +0200

    config/locale.nix: add largeFont option

--- config/locale.nix
+++ config/locale.nix
@@ -1,4 +1,5 @@
 {
+  pkgs,
   config,
   lib,
   azLib,
@@ -11,12 +12,17 @@ in
       enable = optBool false;
       tz = optStr "MET"; # CET/CEST
       keymap = optStr "colemak";
+      largeFont = optBool false;
     };
     config = mkIf cfg.enable {
       time.timeZone = cfg.tz;
       i18n.defaultLocale = "en_US.UTF-8";
       console = {
-        font = "Lat2-Terminus16";
+        font =
+          if cfg.largeFont
+          then "${pkgs.terminus_font}/share/consolefonts/ter-132n.psf.gz"
+          else "Lat2-Terminus16";
+        packages = lib.optional cfg.largeFont pkgs.terminus_font;
         keyMap = lib.mkForce cfg.keymap;
         useXkbConfig = true;
       };
